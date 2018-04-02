0469e5c5a6804060b3de694765ae62b864fc6b5c
Lars Kellogg-Stedman <lars@oddbit.com>
2018-04-02 07:25:28 -0400

    use farenheit

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
-temp1_low = 26
-temp1_high = 32
+def C(f):
+    return (f - 32) * (5/9)
+
+
+temp1_low = C(85)
+temp1_high = C(92)
 temp1_id = 'ds-28ffc018231704dd'
-temp2_low = 23
-temp2_high = 27
+temp2_low = C(75)
+temp2_high = C(80)
 temp2_id = 'ds-28ffaccb221704cf'
 humid_low = 50
 humid_high = 60
