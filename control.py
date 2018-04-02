ce77006047f4bbc417c1e912cc0649d937fa77b3
Lars Kellogg-Stedman <lars@oddbit.com>
2018-04-02 17:54:02 -0400

    a bunch of stuff

--- control.py
+++ control.py
@@ -10,8 +10,21 @@ sensors = {}
 config = {}
 
 
-def request_handler(reader, writer):
-    yield from webserver.handle_request(reader, writer, sensors, config)
+class Event:
+    def __init__(self):
+        self.clear()
+
+    def clear(self):
+        self.flag = False
+
+    def set(self):
+        self.flag = True
+
+    def __await__(self):
+        while not self.flag:
+            yield from asyncio.sleep(0)
+
+    __iter__ = __await__
 
 
 def main():
@@ -19,12 +32,14 @@ def main():
         config.update(json.load(fd))
 
     loop = asyncio.get_event_loop()
+    sensors_ready = Event()
+    ws = webserver.Webserver(sensors, config)
 
-    t_display = tasks.task_display(sensors, config)
-    t_control = tasks.task_control(sensors, config)
-    t_reader = tasks.task_read_sensors(sensors, config)
+    t_display = tasks.task_display(sensors, config, wait_on=sensors_ready)
+    t_control = tasks.task_control(sensors, config, wait_on=sensors_ready)
+    t_reader = tasks.task_read_sensors(sensors, config, notify=sensors_ready)
     t_webserver = asyncio.start_server(
-        request_handler, '0.0.0.0', 80)
+        ws.handle_request, '0.0.0.0', 80)
 
     tasklist = [t_display, t_control, t_reader, t_webserver]
     for task in tasklist:
@@ -38,3 +53,5 @@ def main():
     finally:
         for task in tasklist:
             task.close()
+
+main()
