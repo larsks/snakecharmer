0b48ac92b4cc97680981942f13e15342a6cd74c5
Lars Kellogg-Stedman <lars@oddbit.com>
2018-04-02 11:22:45 -0400

    exchange a onewire bus for an i2c display

--- hardware.py
+++ hardware.py
@@ -2,6 +2,7 @@ import dht
 import ds18x20
 import machine
 import onewire
+import tm1637
 
 W1_PIN_0 = 4
 W1_PIN_1 = 0
@@ -11,14 +12,16 @@ RELAY_HEAT1_PIN = 12
 RELAY_HEAT2_PIN = 13
 RELAY_HUMID_PIN = 14
 
+DISPLAY_CLK_PIN = 5
+DISPLAY_DIO_PIN = 4
 
-w1_0 = onewire.OneWire(machine.Pin(W1_PIN_0))
+# w1_0 = onewire.OneWire(machine.Pin(W1_PIN_0))
 w1_1 = onewire.OneWire(machine.Pin(W1_PIN_1))
-ds_0 = ds18x20.DS18X20(w1_0)
+# ds_0 = ds18x20.DS18X20(w1_0)
 ds_1 = ds18x20.DS18X20(w1_1)
 dht_0 = dht.DHT22(machine.Pin(DHT_PIN_0))
 
-sensors_ds = [ds_0, ds_1]
+sensors_ds = [ds_1]
 sensors_dht = [dht_0]
 
 heat1 = machine.Pin(RELAY_HEAT1_PIN, machine.Pin.OUT, value=1)
@@ -30,3 +33,6 @@ relays = {
     'heat2': heat2,
     'humidifier': humidifier,
 }
+
+display = tm1637.TM1637Decimal(clk=machine.Pin(DISPLAY_CLK_PIN),
+                               dio=machine.Pin(DISPLAY_DIO_PIN))
