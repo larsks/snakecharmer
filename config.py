46c801c27e36005f91a109d99fcb9723fe794bf4
Lars Kellogg-Stedman <lars@oddbit.com>
2018-04-02 11:22:26 -0400

    move temp conversion to utils

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
-def C(f):
-    return (f - 32) * (5/9)
+from utils import F2C
 
-
-temp1_low = C(85)
-temp1_high = C(92)
+temp1_low = F2C(85)
+temp1_high = F2C(92)
 temp1_id = 'ds-28ffc018231704dd'
-temp2_low = C(75)
-temp2_high = C(80)
+temp2_low = F2C(75)
+temp2_high = F2C(80)
 temp2_id = 'ds-28ffaccb221704cf'
 humid_low = 50
 humid_high = 60
